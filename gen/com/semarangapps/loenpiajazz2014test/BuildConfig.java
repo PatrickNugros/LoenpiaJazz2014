28f757a822ff42c7cbb62c564fcfaaf3dd343d9b
Patrick Nugros <patrick@patrick-nugros.(none)>
2014-06-01 22:36:26 +0700

    Commit Pertama

---
+++ gen/com/semarangapps/loenpiajazz2014test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.semarangapps.loenpiajazz2014test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
